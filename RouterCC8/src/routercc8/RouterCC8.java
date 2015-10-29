b29dcc48a8a0bba8492787f4577a0c1355813f83
KevinAlfonso <KevinAlfonso@kags-notepc>
2015-10-28 21:36:42 -0600

    Hello World

--- RouterCC8/src/routercc8/RouterCC8.java
+++ RouterCC8/src/routercc8/RouterCC8.java
@@ -16,6 +16,7 @@ public class RouterCC8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World.");
     }
     
 }
